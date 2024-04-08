58e1671edb7a168e96cd405ac3575d62c269704e
shandeming <calvinzhaomr@163.com>
2024-04-08 20:34:19 +0800

    Refactor Tag page to improve UI and add tags to posts

--- src/app/tag/page.tsx
+++ src/app/tag/page.tsx
@@ -6,16 +6,20 @@ const Tag = () => {
     .flat()
     .filter((tag, index, self) => self.indexOf(tag) === index);
   return (
-    <div className="prose">
-      <h1>Tags</h1>
-      <ul>
-        {tags.map((tag) => (
-          <li key={tag}>
-            <a href={`/tag/${tag}`}>{tag}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="border-b-2 pb-4">
+        <h1 className="text-5xl font-bold">Tags</h1>
+      </div>
+      <div className="prose">
+        <ul className="inline">
+          {tags.map((tag) => (
+            <li key={tag}>
+              <a href={`/tag/${tag}`}>{tag}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
